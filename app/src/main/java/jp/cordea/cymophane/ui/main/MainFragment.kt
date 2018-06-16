beeee8ca9e9dcc9dbcf93332a94c8d4786755af9
Yoshihiro Tanaka <contact@cordea.jp>
2018-06-16 20:23:10 +0900

    bind to view

--- app/src/main/java/jp/cordea/cymophane/ui/main/MainFragment.kt
+++ app/src/main/java/jp/cordea/cymophane/ui/main/MainFragment.kt
@@ -1,12 +1,13 @@
 package jp.cordea.cymophane.ui.main
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import jp.cordea.cymophane.R
+import jp.cordea.cymophane.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -15,16 +16,23 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding
+    private val adapter = CatAdapter()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View = MainFragmentBinding.inflate(inflater, container, false).also {
+        binding = it
+    }.root
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.photos.observe(this, Observer { photos ->
+            adapter.submitList(photos)
+        })
+        binding.recyclerView.adapter = adapter
     }
-
 }
