96c6c5f0836e4f5f68ae9d3656d960a556b416ae
Yoshihiro Tanaka <contact@cordea.jp>
2018-06-17 14:41:08 +0900

    start main activity

--- app/src/main/java/jp/cordea/cymophane/ui/login/LoginActivity.kt
+++ app/src/main/java/jp/cordea/cymophane/ui/login/LoginActivity.kt
@@ -1,9 +1,13 @@
 package jp.cordea.cymophane.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import jp.cordea.cymophane.MainActivity
 import jp.cordea.cymophane.R
 import jp.cordea.cymophane.databinding.ActivityLoginBinding
 
@@ -15,6 +19,13 @@ class LoginActivity : AppCompatActivity() {
                 this,
                 R.layout.activity_login
         )
-        binding.vm = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        binding.vm = viewModel
+        viewModel.shouldGoToMain.observe(this, Observer {
+            if (!it.isNullOrBlank()) {
+                Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        })
     }
 }
--- app/src/main/java/jp/cordea/cymophane/ui/login/LoginViewModel.kt
+++ app/src/main/java/jp/cordea/cymophane/ui/login/LoginViewModel.kt
@@ -1,12 +1,18 @@
 package jp.cordea.cymophane.ui.login
 
 import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
 
+    private val _shouldGoToMain = MutableLiveData<String>()
+    val shouldGoToMain: LiveData<String> = _shouldGoToMain
+
     var key: String = ""
 
     val onClick = View.OnClickListener {
+        _shouldGoToMain.value = key
     }
 }
